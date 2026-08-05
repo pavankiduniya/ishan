8d96bdcf0a33dd75b54be2568c19a0b52b959f31
Pavan19489 <pavan19489@example.com>
2026-08-05 15:51:40 +0530

    Right rail: increase sticky top to 120px so avatar clears the header

--- post.php
+++ post.php
@@ -102,7 +102,7 @@ require_once __DIR__ . '/includes/header.php';
     $aboutInfo = $siteContent['about'];
     $contactInfo = $siteContent['contact'];
     ?>
-    <aside class="right-rail" style="position:sticky;top:96px;align-self:start;">
+    <aside class="right-rail" style="position:sticky;top:120px;align-self:start;">
         <div class="widget author">
             <?php if (!empty($aboutInfo['photo'])): ?>
             <img class="avatar-img" src="<?= e($aboutInfo['photo']) ?>" alt="<?= e($aboutInfo['heading']) ?>">
